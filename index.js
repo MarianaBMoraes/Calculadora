971d29c591e26c9ba9bf7d24996909394c90a1cb
Mariana <marianabmoraes.ef@gmail.com>
2024-07-03 09:07:39 -0300

    feat: adiciona validação dos números

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const prompt = require("prompt-sync")();
 
-let número = Number(prompt("Digite um número:"));
+function calculadora() {
+  let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+  let numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+  if (isNaN(numero1) || isNaN(numero2)) {
+    console.log("Entrada inválida. por favor, digite números válidos");
+  }
+}
